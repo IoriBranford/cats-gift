34ecb1c0e34fb3b8ea78826fa4f019db224e27c6
IoriBranford <ioribranford@gmail.com>
2021-08-28 22:17:04 -0700

    Player hits triggers to activate cat actions. Start with cat appearing and following paths.

--- Cat.gd
+++ Cat.gd
@@ -1,13 +1,35 @@
 extends Area
 
-export var Player:NodePath = ""
-export var speed = 20.0
+export var speed = 10.0
 
-var pathFollow:PathFollow = null
+var paths
 
 func _ready():
-	pathFollow = get_parent() as PathFollow
+	visible = false
+	paths = get_parent().find_node("Paths")
 
-func _process(delta):
+func _physics_process(delta):
+	var pathFollow = get_parent() as PathFollow
 	if pathFollow:
-		pathFollow.offset += delta
+		pathFollow.offset += delta*speed
+		if pathFollow.unit_offset > 1:
+			pathFollow.unit_offset = 1
+
+func reparent(child: Node, new_parent: Node):
+	var old_parent = child.get_parent()
+	old_parent.remove_child(child)
+	new_parent.add_child(child)
+
+func _on_Cat_body_entered(_body):
+	$BellsPlayer.play()
+
+func _on_TriggerCatAppear_body_entered(_body):
+	$BellsPlayer.play()
+	visible = true
+
+func _on_TriggerCatPath_body_entered(_body, path_number):
+	var pathFollow = paths.find_node("PathFollow%d" % path_number)
+	get_parent().remove_child(self)
+	pathFollow.add_child(self)
+	translation = Vector3.ZERO
+	pathFollow.offset = 0
--- ThirdPersonController/Player.gd
+++ ThirdPersonController/Player.gd
@@ -1,13 +1,8 @@
 extends KinematicBody
 
-export var CatPath:NodePath = ""
-
-var Cat
 var footstepSounds = []
 
 func _ready():
-	Cat = get_node(CatPath)
-	Cat.visible = false
 	for i in range(1,18):
 		footstepSounds.append(load("res://Sounds/footsteps/%d.ogg" % i) as AudioStreamOGGVorbis)
 
@@ -15,12 +10,3 @@ func _on_Controller_footstep():
 	$FootstepPlayer.stop()
 	$FootstepPlayer.stream = footstepSounds[randi() % footstepSounds.size()]
 	$FootstepPlayer.play()
-
-func _on_Cat_body_entered(body):
-	if self == body:
-		$BellsPlayer.play()
-
-func _on_TriggerCatAppear_body_entered(body):
-	if self == body:
-		$BellsPlayer.play()
-		Cat.visible = true
