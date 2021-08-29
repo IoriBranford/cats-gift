22bf4624c01a596e021db66b35622b25941057a6
IoriBranford <ioribranford@gmail.com>
2021-08-29 10:17:16 -0700

    Touching cat increases feline level. Level 1 increases ambient light.

--- Player.gd
+++ Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody
 
 var footstepSounds = []
+var felineLevel = 0
 
 func _ready():
 	for i in range(1,18):
@@ -10,3 +11,13 @@ func _on_Controller_footstep():
 	$FootstepPlayer.stop()
 	$FootstepPlayer.stream = footstepSounds[randi() % footstepSounds.size()]
 	$FootstepPlayer.play()
+
+func _on_Cat_body_entered(_body):
+	felineLevel += 1
+	match felineLevel:
+		1:
+			onFelineLevel1()
+
+func onFelineLevel1():
+	var we = get_parent().find_node("WorldEnvironment", false) as WorldEnvironment
+	we.environment.ambient_light_energy = 8
