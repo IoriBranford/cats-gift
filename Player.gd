7c943589e6db2dfc2b79293bc9a4b8c80008427e
IoriBranford <ioribranford@gmail.com>
2021-09-04 16:26:49 -0700

    Restart project with Qodot and new character model.

---
+++ Player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody
+
+var footstepSounds = []
+var felineLevel = 0
+
+func _ready():
+	$Ears.visible = false
+	$Tail.visible = false
+	for i in range(1,18):
+		footstepSounds.append(load("res://sounds/footsteps/%d.ogg" % i) as AudioStreamOGGVorbis)
+	$Model/AnimationPlayer.play("idle-loop")
+
+func _on_Controller_footstep():
+	$FootstepPlayer.stop()
+	$FootstepPlayer.stream = footstepSounds[randi() % footstepSounds.size()]
+	$FootstepPlayer.play()
+
+func _on_Cat_body_entered(_body):
+	if $Timer.time_left > 0:
+		return
+	$Timer.start()
+	felineLevel += 1
+	match felineLevel:
+		1:
+			onFelineLevel1()
+		2:
+			onFelineLevel2()
+
+func onFelineLevel1():
+	$Ears.visible = true
+	$Ears/CPUParticles.restart()
+	var we = get_node("../WorldEnvironment") as WorldEnvironment
+	we.environment.ambient_light_energy = 2
+	var cat = get_parent().find_node("Cat", true, false)
+	cat.use_pathfollow(3)
+
+func onFelineLevel2():
+	$Tail.visible = true
+	$Tail/CPUParticles.restart()
+	$Controller.MaxJump = 25
+	$Controller.MaxFloorAngle = 60
+	$SpotLight.spot_range = 35
---
+++ ThirdPersonController/Controller.gd
@@ -0,0 +1,125 @@
+extends Spatial
+
+#const Tightrope = preload("res://Tightrope.gd")
+
+export(NodePath) var PlayerPath  = "" #You must specify this in the inspector!
+export(float) var MovementSpeed = 10
+export(float) var Acceleration = 3
+export(float) var MaxJump = 19
+export(float) var MouseSensitivity = 2
+export(float) var RotationLimit = 45
+export(float) var MaxZoom = 0.5
+export(float) var MinZoom = 1.5
+export(float) var ZoomSpeed = 2
+export var MaxFloorAngle = PI/4
+
+var Player
+var InnerGimbal
+var Rotation = Vector2()
+var gravity = -10
+var Movement = Vector3()
+var ZoomFactor = MaxZoom
+var ActualZoom = MaxZoom
+var Speed = Vector3()
+var CurrentVerticalSpeed = Vector3()
+var JumpAcceleration = 3
+var IsAirborne = false
+var footstepTimer = 0
+
+signal footstep
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	Player = get_node(PlayerPath)
+	InnerGimbal =  $InnerGimbal
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion :
+		Rotation += event.relative
+	
+	if event is InputEventMouseButton:
+		match event.button_index:
+			BUTTON_WHEEL_UP:
+				ZoomFactor -= 0.05
+			BUTTON_WHEEL_DOWN:
+				ZoomFactor += 0.05
+		ZoomFactor = clamp(ZoomFactor, MaxZoom, MinZoom)
+	if event is InputEventKey and event.pressed:
+		match event.scancode:
+			KEY_ESCAPE:
+				get_tree().quit()
+
+func _physics_process(delta):
+	var Direction = Vector3.ZERO
+	if Input.is_action_pressed("ui_up"):
+		Direction.z += 1
+	if Input.is_action_pressed("ui_down"):
+		Direction.z -= 1
+	if Input.is_action_pressed("ui_left"):
+		Direction.x += 1
+	if Input.is_action_pressed("ui_right"):
+		Direction.x -= 1
+	if Input.is_action_just_pressed("ui_accept"):
+		if not IsAirborne:
+			CurrentVerticalSpeed = Vector3(0,MaxJump,0)
+			IsAirborne = true
+			emit_signal("footstep")
+
+	#Rotation
+	Player.rotate_y(deg2rad(-Rotation.x)*delta*MouseSensitivity)
+#	Player.rotate_y(deg2rad(-Direction.x)*delta*MouseSensitivity)
+	InnerGimbal.rotate_x(deg2rad(Rotation.y)*delta*MouseSensitivity)
+	InnerGimbal.rotation_degrees.x = clamp(InnerGimbal.rotation_degrees.x, -RotationLimit, RotationLimit)
+	Rotation = Vector2()
+	
+	#Movement
+	var MaxSpeed = MovementSpeed *Direction.normalized()
+	Speed = Speed.linear_interpolate(MaxSpeed, delta * Acceleration)
+	Movement = Player.transform.basis * (Speed)
+	CurrentVerticalSpeed.y += gravity * delta * JumpAcceleration
+	Movement += CurrentVerticalSpeed
+	
+	var tightrope
+	var tightropepoint
+#	if $RayCast.is_colliding():
+#		tightrope = $RayCast.get_collider() as Tightrope
+#		if tightrope:
+#			tightropepoint = $RayCast.get_collision_point()
+#			Player.translation = tightropepoint
+#			var vec = Vector3.FORWARD
+#			vec = vec.rotated(Vector3.RIGHT, tightrope.rotation.x)
+#			vec = vec.rotated(Vector3.UP, tightrope.rotation.y)
+#			vec = vec.rotated(Vector3.FORWARD, tightrope.rotation.z)
+#			Movement = Movement.dot(vec) * vec
+
+	var wasOnFloor = Player.is_on_floor()
+	Player.move_and_slide(Movement,Vector3.UP, false, 4, MaxFloorAngle)
+	if Player.is_on_floor() :
+		if not wasOnFloor:
+			emit_signal("footstep")
+		CurrentVerticalSpeed.y = 0
+		IsAirborne = false
+
+		var speed = Speed.length()
+		if speed >= 1:
+			footstepTimer += delta
+			var footstepInterval = clamp(1 - speed/MovementSpeed, 0.25, 0.75)
+			if footstepTimer >= footstepInterval:
+				emit_signal("footstep")
+				footstepTimer = 0
+		else:
+			footstepTimer = 0
+
+	#Zoom
+	ActualZoom = lerp(ActualZoom, ZoomFactor, delta * ZoomSpeed)
+	InnerGimbal.set_scale(Vector3(ActualZoom,ActualZoom,ActualZoom))
+
+
+#func _on_TightropeDetector_body_entered(body):
+#	var tightrope = body as Tightrope
+#	if tightrope:
+#		var vec = Vector3.FORWARD
+#		vec = vec.rotated(Vector3.RIGHT, tightrope.rotation.x)
+#		vec = vec.rotated(Vector3.UP, tightrope.rotation.y)
+#		vec = vec.rotated(Vector3.FORWARD, tightrope.rotation.z)
+		
