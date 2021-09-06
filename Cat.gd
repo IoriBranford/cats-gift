884c08fe525d4991083346de89b40a909382cbd9
IoriBranford <ioribranford@gmail.com>
2021-09-05 18:38:46 -0700

    Simplify and fix cat path following. Lower his speed to 10. Place his first path.

--- Cat.gd
+++ Cat.gd
@@ -1,30 +1,26 @@
 extends Area
 
-export var speed = 20.0
+export var speed = 10.0
 
 var pathfollow:PathFollow = null
 var pathfollow_dir = 1
 
-
 func _physics_process(delta):
+	var newpos = translation
 	var dpos = Vector3.ZERO
 	if pathfollow:
 		pathfollow.offset += delta*speed*pathfollow_dir
 		pathfollow.unit_offset = clamp(pathfollow.unit_offset, 0, 1)
-		
-		var pos = to_global(translation)
-		var dest = pathfollow.to_global(pathfollow.translation)
-		var dist = dest - pos
-		var velocity = speed * dist.normalized()
-		dpos = velocity * delta
-		dpos = Vector3(move_toward(pos.x, dest.x, dpos.x), move_toward(pos.y, dest.y, dpos.y), move_toward(pos.z, dest.z, dpos.z))
-		look_at(dest, Vector3.UP)
-		
-	translation += dpos
-	if dpos.length_squared() > 1:
+		newpos = pathfollow.translation
+		dpos = newpos - translation
+	if dpos.length_squared() > 0:
 		$Model/AnimationPlayer.play("run-loop")
+		look_at(newpos, Vector3.UP)
 	else:
 		$Model/AnimationPlayer.play("idle-loop")
+		var player = get_node("../Player")
+		look_at(player.translation, Vector3.UP)
+	translation = newpos
 
 func _on_Cat_body_entered(_body):
 	$BellsPlayer.play()
@@ -33,15 +29,16 @@ func _on_TriggerCatAppear_body_entered(_body):
 	$BellsPlayer.play()
 	visible = true
 
-func _on_TriggerCatPath_body_entered(_body, path_number):
-	pass
+func _on_TriggerCatPath_body_entered(_body, nodepath):
+	start_path(get_node(nodepath))
 
 func start_path(path:Path):
 	var points = path.curve.get_baked_points()
-	var point0 = path.translation + points[0]
-	var pointn = path.translation + points[points.size() - 1]
-	var dist0 = point0 - translation
-	var distn = pointn - translation
+	var point0 = path.to_global(points[0])
+	var pointn = path.to_global(points[points.size() - 1])
+	var global_translation = to_global(translation)
+	var dist0 = point0 - global_translation
+	var distn = pointn - global_translation
 	pathfollow = path.get_node("PathFollow")
 	if distn.length_squared() < dist0.length_squared():
 		pathfollow.unit_offset = 1
